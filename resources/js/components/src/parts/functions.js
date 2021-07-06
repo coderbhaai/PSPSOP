610becdafdbc35936ff93167148779c18e8380f6
arsnavneet <amit.khare588@gmail.com>
2021-07-06 10:55:11 +0530

    `commit`

--- resources/js/components/src/parts/functions.js
+++ resources/js/components/src/parts/functions.js
@@ -2,5 +2,5 @@ export const base = '/'
 export function callSwal(mesg){ swal({ title: mesg, timer: 4000 }) }
 export function printError(mesg){ console.log('mesg', mesg) }
 export const time = new Date().toISOString().slice(0, 19).replace('T', ' ')
-export const imgPath = "/storage/"
-// export const imgPath = "/saukhyam/storage/app/public/"
+// export const imgPath = "/storage/"
+export const imgPath = "/psp/storage/app/public/"
